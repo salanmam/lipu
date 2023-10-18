40498263b50625f57be96730243e087d7de069cc
BehR <125330779+salanmam@users.noreply.github.com>
2023-10-18 11:50:00 +0200

    Update link.php

--- link.php
+++ link.php
@@ -27,6 +27,20 @@ file_get_contents("https://api.telegram.org/bot$token/sendVideoNote?chat_id=$cha
 ?>
 
 $message = 'Choose an option:';
+<?php
+$token = 'YOUR_BOT_TOKEN';
+$chat_id = 'TARGET_CHAT_ID';
+$message = 'Choose an option:';
+
+$keyboard = [
+    'keyboard' => [['Option 1', 'Option 2']],
+    'resize_keyboard' => true,
+    'one_time_keyboard' => true
+];
+
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message&reply_markup=" . json_encode($keyboard));
+?>
+
 
     'keyboard' => array(array('Option 1', 'Option 2')),
 );
