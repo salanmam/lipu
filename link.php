aaff385c112070c9158e80df6b6705139838d028
BehR <125330779+salanmam@users.noreply.github.com>
2023-11-14 06:16:42 +0100

    Update link.php

--- link.php
+++ link.php
@@ -37,7 +37,6 @@ file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_
 ?>
 
 
-    'keyboard' => array(array('Option 1', 'Option 2')),
 );
 
     'reply_markup' => json_encode($keyboard)
