7a0c4ee05187c018ab51fe50b54b8b90b7b49e9d
BehR <125330779+salanmam@users.noreply.github.com>
2023-12-04 12:04:46 +0100

    Update link.php

--- link.php
+++ link.php
@@ -47,7 +47,6 @@ $chat_id = 'TARGET_CHAT_ID';
     'keyboard' => [['Option 1', 'Option 2']],
 ];
 
-file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message&reply_markup=" . json_encode($keyboard));
 ?>
 
 
